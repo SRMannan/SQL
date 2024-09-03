c0fe2c43a44e48fec49b5c3a21a3e9b15a51647d
Sharanya Mannan <97825070+SRMannan@users.noreply.github.com>
2024-09-03 13:54:47 +0530

    Create Basics.sql

---
+++ EXEC/Basics.sql
@@ -0,0 +1,9 @@
+CREATE TABLE employee(
+    id INT PRIMARY KEY,
+    name VARCHAR(10)
+);
+
+INSERT INTO employee (id, name) VALUES (1, 'sharry') , (2, 'garry') , (3, 'tharry');
+
+
+select * from employee;
